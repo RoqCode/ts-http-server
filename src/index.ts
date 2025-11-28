5ba05ca9bfb4cade60f0d8c8eb67074d72930cb3
Hajo Haas <hajo.haas@hmmh.de>
2025-11-28 20:27:57 +0100

    serve file from public dir

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 
 const app = express();
-const port = 8080;
+const PORT = 8080;
 
-app.listen(port, () => {
-  console.log(`server started, listening on 8080`);
+app.use(express.static("./public/"));
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
