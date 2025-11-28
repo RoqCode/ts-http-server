520a8955175663c2c3659b3ea05eb20d141c7427
Hajo Haas <hajo.haas@hmmh.de>
2025-11-28 21:10:07 +0100

    restructure app and add readiness handler

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import { handlerReadiness } from "./lib/handler/readiness.js";
 
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("./public/"));
-
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+app.use("/app", express.static("./src/app"));
+
+app.get("/healthz", handlerReadiness);
