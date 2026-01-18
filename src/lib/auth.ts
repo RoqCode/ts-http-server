66c6d1f40b01be884ce21f818cd02e6b3707b049
Hajo Haas <hajohaas@gmx.de>
2026-01-18 15:31:37 +0100

    add jwt handler

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,6 @@
 import * as argon2 from "argon2";
+import jwt from "jsonwebtoken";
+import { BadRequestError } from "./errors/errors.js";
 
 export function hashPassword(password: string): Promise<string> {
   return argon2.hash(password);
@@ -9,4 +11,20 @@ export function checkPasswordHash(
   hash: string,
 ): Promise<boolean> {
   return argon2.verify(hash, password);
+}
+
+export function validateJWT(tokenString: string, secret: string): string {
+  try {
+    const token = jwt.verify(tokenString, secret);
+
+    if (!token.sub || typeof token.sub !== "string") {
+      console.error("no userID found in token");
+      throw new BadRequestError("no userID found in token");
+    }
+
+    return token.sub;
+  } catch (e) {
+    console.warn(e);
+    throw new BadRequestError("token could not be verified");
+  }
 }
