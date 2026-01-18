47a582dc8885996ee9ef5ac0e154b3a3068a9c26
Hajo Haas <hajohaas@gmx.de>
2026-01-18 15:31:37 +0100

    add jwt handler

---
+++ src/lib/util/makeJWT.ts
@@ -0,0 +1,38 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { BadRequestError } from "../errors/errors.js";
+
+type Payload = Pick<JwtPayload, "iss" | "sub" | "iat" | "exp">;
+
+export function makeJWT(
+  userID: string,
+  expiresIn: number,
+  secret: string,
+): string {
+  const iat = Math.floor(Date.now() / 1000);
+  const exp = iat + expiresIn;
+
+  const payload: Payload = {
+    iss: "chiry",
+    sub: userID,
+    iat,
+    exp,
+  };
+
+  return jwt.sign(payload, secret);
+}
+
+export function validateJWT(tokenString: string, secret: string): string {
+  try {
+    const token = jwt.verify(tokenString, secret);
+
+    if (!token.sub || typeof token.sub !== "string") {
+      console.error("no userID found in token");
+      throw new BadRequestError("no userID found in token");
+    }
+
+    return token.sub;
+  } catch (e) {
+    console.warn(e);
+    throw new BadRequestError("token could not be verified");
+  }
+}
